346de9250a3be2adf101d8a706f8ec36a6be357f
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-18 19:14:04 +0500

    feat: create CustomUser model with user_type field for role-based access control

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    user_type_choices = [
+        ('employer', 'Employer'),
+        ('seeker', 'Job Seeker'),
+        ('admin', 'Admin'),
+    ]
+    user_type = models.CharField(max_length=20, choices=user_type_choices, default='seeker')
