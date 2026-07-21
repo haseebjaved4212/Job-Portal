86a65a29ff08beccea9d9dfed6c670a70525d331
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-21 08:55:10 +0500

    feat: implement signup view for user registration using CustomCreationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomCreationForm()
+    return render(request, 'signup.html', {'form': form})
